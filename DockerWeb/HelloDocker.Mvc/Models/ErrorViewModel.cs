c3d55c615a01c36f3ee49cc8ba2603cd5925b957
Smithy <shaun@softwaresmith.co.uk>
2023-02-12 22:04:11 +0000

    Add 2 proje cts with proper cors policy

--- DockerWeb/HelloDocker.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace HelloDocker.Mvc.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
