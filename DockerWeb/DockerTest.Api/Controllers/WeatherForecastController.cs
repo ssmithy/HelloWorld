6991ac9f0b2abb3698d0cb36f2b8a43ac0c7b740
Smithy <shaun@softwaresmith.co.uk>
2023-02-13 20:30:03 +0000

    play with docker and the api controller

--- DockerWeb/DockerTest.Api/Controllers/WeatherForecastController.cs
+++ DockerWeb/DockerTest.Api/Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ namespace DockerTest.Api.Controllers {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
